a087bc30d5aa600b9153db71ec48e7bc38a075e0
Ajay Ghosh <87318836+SESA475978@users.noreply.github.com>
2024-02-08 23:26:59 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Application for AWS Amplify | AG
+          Application for AWS Amplify | Ajay Ghosh
         </p>
         <a
           className="App-link"
